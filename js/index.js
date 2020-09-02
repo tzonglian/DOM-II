61a5a9b40d9a7d3398a83f9fafa92c1664c1828a
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-09-01 21:24:36 -0400

    MVP Complete.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Your code goes here
+// 10 Unique Events
 
 //#1 - Double-click -> Text: Signed up!
 const signUp = document.querySelectorAll('.btn');
@@ -17,25 +17,78 @@ Array.from(signUp).forEach(function (butt) {
 const gonePic = document.querySelector( '[src= "img/destination.jpg"]');
 gonePic.addEventListener('mouseover', (event) => {
     gonePic.style.opacity = "0.01";
-})
+});
 
-//#3 Resize -> Text Italicizes
+//#3 Move Mouse Out -> Text Italicizes
 const textItalicize = document.querySelector('p');
-textItalicize.addEventListener('resize', () => {
+textItalicize.addEventListener('mouseout', (event) => {
     textItalicize.style.fontStyle = 'italic';
 });
 
-// keydown
-// wheel
-// load
-// focus
-// resize
-// scroll
-// select
-// dblclick
-// drag / drop
-// Note: Drag and drop is a bit more advanced than the others: it's not actually a single type of event but several types that need to work together.
+//#4 Move Mouse on First Paragraph -> Text bolds
+const textMove = document.querySelector('p');
+textMove.addEventListener('mousemove', (event) => {
+    textMove.style.fontWeight = 'bold';
+});
+
+//#5 Right click -> Text blues
+const textBlued = document.querySelector('p');
+textBlued.addEventListener('contextmenu', (event) => {
+    textBlued.style.color = 'darkblue';
+});
+
+//#6 Click with non-standard button -> Text greens
+const textGreened = document.querySelector('p');
+textGreened.addEventListener('auxclick', (event) => {
+    textGreened.style.color = "green";
+});
+
+//#7 Mousedown -> Text reds
+const textRed = document.querySelector('h1');
+textRed.addEventListener('mousedown', (event) => {
+    textRed.style.color = 'darkred';
+});
+
+//#8 Mouseup -> Text purples
+const textPurples = document.querySelector('h1');
+textPurples.addEventListener('mouseup', (event) => {
+    textPurples.style.color = 'purple';
+});
+
+//#9 Select Text -> Text becomes boxed
+const textBoxed = document.querySelector('h1');
+textBoxed.addEventListener('select', (event) => {
+    textBoxed.style.border = 'thin dotted slategray'
+});
+
+//#10 Wheel over picture -> Changes Opacity
+const faded2 = document.querySelector('[src= "img/adventure.jpg"]');
+faded2.addEventListener('wheel', (event) => {
+    faded2.style.opacity = "0.4";
+});
+
 
 //  Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
 
+//mouseout bubbles, mouseleave does not
+const bubbleFuchsia = document.querySelector('h2'); //selects 'Welcome to the Fun Bus!'
+const noBubblePink = document.querySelector('.intro'); //selects 'intro class'
+bubbleFuchsia.addEventListener('mouseleave', (event) => {
+    bubbleFuchsia.style.color = 'fuchsia'; //dark-pink
+    if (true) {
+        noBubblePink.addEventListener('mouseout', (event) => {
+            noBubblePink.style.color = 'pink'; //light-pink
+        });
+    }
+});
+
+//This prevents mouseout from bubbling properly
+
+
 //  Stop the navigation items from refreshing the page by using preventDefault()
+Array.from(document.links).forEach(element => {
+    element.addEventListener('click', event => {
+        event.preventDefault() ;
+    })
+  })
+
